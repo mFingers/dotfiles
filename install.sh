73363c8c07528a8f2cb34c503d95cf527ad200ed
Mike Falanga <m.falanga@gmail.com>
2023-03-01 12:06:09 -0500

    WIP install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install packages and applications
+brew bundle
+
+# Setup symlinks
+export DOTFILES_DIR
