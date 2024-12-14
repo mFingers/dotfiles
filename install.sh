fb9998eb0e1de3d70cecc87a68824d70694dd892
Mike Falanga <m.falanga@gmail.com>
2024-12-14 16:12:47 -0500

    Add iTerm2 Settings

--- install.sh
+++ install.sh
@@ -8,4 +8,11 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 brew bundle
 
 # Setup symlinks
+DOTFILES_DIR=$HOME/.dotfiles
 export DOTFILES_DIR
+
+## iTerm2
+# Specify the preferences directory
+# defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "$DOTFILES_DIR/iterm2"
+# Tell iTerm2 to use the custom preferences in the directory
+# defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
